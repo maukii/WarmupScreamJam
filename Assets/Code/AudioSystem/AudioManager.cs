70853f5ddb383cc7d656386c0865413f53c3ea21
keyesque <nocturneop55n@gmail.com>
2025-09-14 09:05:59 +0200

    added faders and music playability

--- Assets/Code/AudioSystem/AudioManager.cs
+++ Assets/Code/AudioSystem/AudioManager.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class AudioManager : MonoBehaviour
 {
@@ -11,6 +13,9 @@ public class AudioManager : MonoBehaviour
 
     [Header("SFX")]
     public List<AudioData> sfxList = new List<AudioData>();
+    public List<MusicData> musicList = new List<MusicData>();
+
+    Coroutine fadeMusic;
 
     void Awake()
     {
@@ -23,12 +28,17 @@ public class AudioManager : MonoBehaviour
         instance = this;
     }
 
+    // Audio Manager
     public void PlayAudio(string name)
     {
         AudioData audio = FindAudio(name);
 
         if (audio != null)
         {
+            // Set attributes
+            instance.sfxSource.volume = audio.volume;
+            instance.sfxSource.pitch = audio.pitch;
+
             instance.sfxSource.PlayOneShot(audio.clip);
         }
     }
@@ -42,5 +52,64 @@ public class AudioManager : MonoBehaviour
             }
         }
         return null;
+    }
+    // Music Manager
+    public void PlayMusic(string title)
+    {
+        MusicData music = FindMusic(title);
+
+        if (music != null)
+        {
+            // Set attributes
+            instance.musicSource.volume = music.volume;
+            instance.musicSource.resource = music.clip;
+
+            instance.musicSource.Play(1);
+        }
+    }
+    private MusicData FindMusic(string title)
+    {
+        foreach (MusicData music in musicList)
+        {
+            if (name == music.title)
+            {
+                return music;
+            }
+        }
+        return null;
+    }
+
+    // Fade Out
+    public void FadeOutMusic(float interval)
+    {
+        fadeMusic = StartCoroutine(FadeOut(interval));
+    }
+    IEnumerator FadeOut(float interval)
+    {
+        float volume = instance.musicSource.volume;
+
+        while (volume > 0)
+        {
+            volume -= 0.1f;
+            yield return new WaitForSeconds(interval);
+        }
+    }
+    // Fade In
+    public void FadeInMusic(string name, float interval)
+    {
+        PlayMusic(name);
+        instance.musicSource.volume = 0f;
+        fadeMusic = StartCoroutine(FadeIn(interval));
+    }
+    IEnumerator FadeIn(float interval)
+    {
+        float volume = 0.0f;
+        float targetVolume = instance.musicSource.volume;
+
+        while (volume < targetVolume)
+        {
+            volume += 0.1f;
+            yield return new WaitForSeconds(interval);
+        }
     }
 }
---
+++ Assets/Code/AudioSystem/MusicData.cs
@@ -0,0 +1,15 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+[System.Serializable]
+public class MusicData
+{
+    public string title;
+    [Range(0f, 2.0f)]
+    public float volume;
+    public AudioClip clip;
+
+    public MusicData()
+    {
+        this.volume = 1.0f;
+    }
+}
